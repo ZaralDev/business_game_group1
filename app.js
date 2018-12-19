6a08c312bacf6a27a32c1b197a87191e3c1f3477
Zaral <samuelmarchese7@gmail.com>
2018-12-19 11:21:55 +0100

    create navbar

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ var routes = [
 function initExpress() {
   // view engine setup
   app.set('views', path.join(__dirname, '/views'));
-  app.set('view engine', 'html');
+  app.set('view engine', 'ejs');
   app.use(logger('dev'));
   app.use(express.json());
   app.use(express.urlencoded({extended: false}));
